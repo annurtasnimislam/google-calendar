1509dd89c1edb87be72948febde32943142e4b64
annurtasnimislam <annurtasnimislam@gmail.com>
2024-01-10 17:32:43 +0600

    feat: design complete

---
+++ src/components/Home/DateBox/DateBox.jsx
@@ -0,0 +1,9 @@
+import classes from "./DateBox.module.css";
+
+export default function DateBox() {
+  return (
+    <div className={classes.dateBox}>
+      <p>31</p>
+    </div>
+  );
+}
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,44 @@
+import DateBox from "./DateBox/DateBox";
+import classes from "./Home.module.css";
+
 export default function Home() {
   return (
-    <div>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, quibusdam
-      dolorem. Nam officia perspiciatis dignissimos nisi voluptas non, placeat
-      quo repudiandae repellendus omnis nobis qui sapiente consectetur fugiat
-      nesciunt hic sed modi quisquam iure reiciendis. Dolores placeat rem, at
-      tempore dicta inventore quisquam recusandae iusto consequuntur facilis
-      nostrum iure libero.
+    <div className={classes.home}>
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
+      <DateBox />
     </div>
   );
 }
