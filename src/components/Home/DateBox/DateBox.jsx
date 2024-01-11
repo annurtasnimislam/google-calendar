39c5ec8e9397764860aeb484030f5eb95e9f4beb
annurtasnimislam <annurtasnimislam@gmail.com>
2024-01-11 16:24:52 +0600

    feat: current date highlight in leftbar & center

--- src/components/Home/DateBox/DateBox.jsx
+++ src/components/Home/DateBox/DateBox.jsx
@@ -2,10 +2,14 @@ import classes from "./DateBox.module.css";
 import { getDate, format } from "date-fns";
 
 export default function DateBox({ date, index }) {
+  let currentDate = getDate(new Date());
+
   return (
     <div className={classes.dateBox}>
       {index < 7 && <p>{format(date, "EEE")}</p>}
-      <p>{getDate(date)}</p>
+      <p className={getDate(date) === currentDate ? classes.dateActive : ""}>
+        {getDate(date)}
+      </p>
     </div>
   );
 }
--- src/components/Layout/LeftSide/Text/Text.jsx
+++ src/components/Layout/LeftSide/Text/Text.jsx
@@ -2,9 +2,15 @@ import classes from "./Text.module.css";
 import { getDate, format } from "date-fns";
 
 export default function Text({ date, day }) {
+  let currentDate = getDate(new Date());
+
   return (
-    <p className={classes.text}>
-      {day ? format(date, "E").charAt(0) : getDate(date)}
-    </p>
+    <div
+      className={
+        getDate(date) === currentDate ? classes.dateActive : classes.text
+      }
+    >
+      <p>{day ? format(date, "E").charAt(0) : getDate(date)}</p>
+    </div>
   );
 }
